3e92b13e6b944fe2438d746e57ef14ee4264db99
Yash-Mehta-69 <yashm8866@gmail.com>
2024-08-18 21:16:06 +0530

    computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,33 +5,29 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  try {
-    const computer = useGLTF("./pc/scene.gltf");
+  const computer = useGLTF("./pc/scene.gltf");
 
-    return (
-      <mesh>
-        <hemisphereLight intensity={1} groundColor='black' />
-        <spotLight
-          position={[-20, 50, 10]}
-          angle={0.12}
-          penumbra={1}
-          intensity={1}
-          castShadow
-          shadow-mapSize={1024}
-        />
-        <pointLight intensity={2} />
-        <primitive
-          object={computer.scene}
-          scale={isMobile ? 0.29 : 0.59}
-          position={isMobile ? [0, -2, -0.6] : [0, -3.25, -1.5]}
-          rotation={[-0.01, -0.2, -0.1]}
-        />
-      </mesh>
-    );
-  } catch (error) {
-    console.error("Failed to load GLTF model:", error);
-    return null;
-  }
+  return (
+    <mesh>
+      <hemisphereLight intensity={1} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={2} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.29 : 0.59}
+        position={isMobile ? [0, -2, -0.6] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+        rotation-y={0}
+      />
+    </mesh>
+  );
 };
 
 const ComputersCanvas = () => {
