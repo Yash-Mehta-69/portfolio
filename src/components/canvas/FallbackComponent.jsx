287455419170820e5c01b9b3831c8090ad7a299a
Yash-Mehta-69 <yashm8866@gmail.com>
2024-08-19 12:20:15 +0530

    hope2

---
+++ src/components/canvas/FallbackComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Fallback.css'
+
+const FallbackComponent = () => {
+  return (
+    <div className="fallback-container">
+      <p>Model could not be loaded.</p>
+      {/* Optionally, include a placeholder image or other content */}
+      <img src="/path/to/placeholder-image.png" alt="Placeholder" />
+    </div>
+  );
+};
+
+export default FallbackComponent;
