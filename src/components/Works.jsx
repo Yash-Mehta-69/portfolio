ef7e19334df263ae057f413d091f7a5ede99ad13
Yash-Mehta-69 <yashm8866@gmail.com>
2024-08-24 19:09:29 +0530

    project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -64,7 +64,11 @@ const Works = () => {
 
     <div className="w-full flex">
       <motion.p variants={fadeIn("","","",0.1,1)} className="mt-2 text-secondary text-[17px] max-w-4xl leading=[30px]">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, officia fugit fuga praesentium ab fugiat consequatur incidunt et blanditiis voluptatum. Voluptate illum quae odio qui quaerat, ad beatae eius? Praesentium, delectus repudiandae?
+        Following projects showcases my skills and experience through
+        real-world examples of my work. Each project is briefly described with
+        links to code repositories and live demos in it. It reflects my
+        ability to solve complex problems, work with different technologies,
+        and manage projects effectively.      
       </motion.p>
     </div>
 
